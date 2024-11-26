4eac17911a29b3a5863c8db7f7347fe6c5166bcc
moosa khan <moosakhanwp@gmail.com>
2024-11-26 18:22:04 +0500

    Initial commit

---
+++ src/components/content.js
@@ -0,0 +1,5 @@
+function Content() {
+    return <p>This is the main content of the page.</p>;
+}
+
+export default Content;
---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <h1>Welcome to My React App!</h1>;
+}
+
+export default Header;
