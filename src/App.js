052ab04a69e48a430c7e1459086964b7680b9192
moosa khan <moosakhanwp@gmail.com>
2024-12-02 11:16:54 +0500

    Updated files for task8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './styles/App.css'; // Import global styles
-import Navbar from './components/navbar';
-import LandingPage from './components/landingpage';
-import Footer from './components/footer';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <LandingPage />
-      <Footer />
+      
     </div>
   );
 }
